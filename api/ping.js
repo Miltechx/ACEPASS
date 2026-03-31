f4224ff0804a9164fc6682d64ef07f070951a3f0
CyberEsbron <92678085+Miltechx@users.noreply.github.com>
2026-03-31 10:59:19 +0100

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,1 +1,8 @@
-
+module.exports = function handler(req, res) {
+  const key = process.env.GROQ_API_KEY || '';
+  res.status(200).json({
+    status: 'ok',
+    groq_key_set: key.length > 0,
+    key_preview: key.length > 8 ? key.slice(0,8)+'...' : 'NOT SET'
+  });
+};
